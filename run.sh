01e84d960338c49bea9ed2aa2c946bb4902c5d8c
Your Name <you@example.com>
2025-01-06 07:21:09 +0000

    ..

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ export HF_TOKEN=$(bash -ic 'source ~/.bashrc; echo $HF_TOKEN')
 . ~/venv/bin/activate
 # pip install datasets
 # pip install --find-links https://download.pytorch.org/whl/torch_stable.html torch==2.3.1+cpu
+# pip install -U "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+
 python notebooks/sft_trainer.py
