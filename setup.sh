57a16e96a9b5c9a3b01d763e83911b7fe03d6c98
Your Name <you@example.com>
2025-01-06 06:28:36 +0000

    ..

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# sudo apt-get install -y -qq software-properties-common
+# sudo add-apt-repository -y ppa:deadsnakes/ppa
+# sudo apt-get install -y -qq python3.11-full python3.11-dev
+
+# python3.11 -m venv ~/venv
+sudo docker build -t easydel-base -f Dockerfile .
