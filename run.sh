de45ce9ebfbbc135c5931f2652853fcf58f11fa8
Your Name <you@example.com>
2025-01-06 07:10:08 +0000

    ..

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 export HF_TOKEN=$(bash -ic 'source ~/.bashrc; echo $HF_TOKEN')
 
 . ~/venv/bin/activate
-pip install datasets
+# pip install datasets
+# pip install --find-links https://download.pytorch.org/whl/torch_stable.html torch==2.3.1+cpu
 python notebooks/sft_trainer.py
